5de308750d58205323497303c55b1d7e5d608b67
Cláudio Roberto <claudioroberto0707@outlook.com>
2025-07-11 23:22:27 -0300

    incluindo um navLink no logo Fintech para direcionar o usuario ao inicio da page

--- src/Components/Sidenav/index.tsx
+++ src/Components/Sidenav/index.tsx
@@ -4,47 +4,49 @@ import FintechSVG from '../../assets/FintechSVG'
 
 const index = () => {
   return (
-   <nav className='sidenav box bg-'>
-  <FintechSVG title="Fintech Logo"/>
-    <ul>
-      <li>
-        <span>
-          <img src={icons.resumo} alt="resumo" />
-        </span>
-        <NavLink to="/">Resumo</NavLink>
-      </li>
-      <li>
-        <span>
-          <img src={icons.vendas} alt="vendas" />
-        </span>
-        <NavLink to="/vendas">Vendas</NavLink>
-      </li>
-      <li>
-        <span>
-          <img src={icons.webhooks} alt="web" />
-        </span>
-        <NavLink to="/">WebHooks</NavLink>
-      </li>
-      <li>
-        <span>
-          <img src={icons.configuracoes} alt="configuraçõe" />
-        </span>
-        <NavLink to="/">Configuraçõpes</NavLink>
-      </li>
-      <li>
-        <span>
-          <img src={icons.contato} alt="contato" />
-        </span>
-        <NavLink to="/">Contato</NavLink>
-      </li>
-      <li>
-        <span>
-          <img src={icons.sair} alt="sair" />
-        </span>
-        <NavLink to="/">Sair</NavLink>
-      </li>
-    </ul>
-   </nav>
+    <nav className='sidenav box bg-'>
+      <NavLink to="/">
+        <FintechSVG title="Fintech Logo" />
+      </NavLink>
+      <ul>
+        <li>
+          <span>
+            <img src={icons.resumo} alt="resumo" />
+          </span>
+          <NavLink to="/">Resumo</NavLink>
+        </li>
+        <li>
+          <span>
+            <img src={icons.vendas} alt="vendas" />
+          </span>
+          <NavLink to="/vendas">Vendas</NavLink>
+        </li>
+        <li>
+          <span>
+            <img src={icons.webhooks} alt="web" />
+          </span>
+          <NavLink to="/">WebHooks</NavLink>
+        </li>
+        <li>
+          <span>
+            <img src={icons.configuracoes} alt="configuraçõe" />
+          </span>
+          <NavLink to="/">Configuraçõpes</NavLink>
+        </li>
+        <li>
+          <span>
+            <img src={icons.contato} alt="contato" />
+          </span>
+          <NavLink to="/">Contato</NavLink>
+        </li>
+        <li>
+          <span>
+            <img src={icons.sair} alt="sair" />
+          </span>
+          <NavLink to="/">Sair</NavLink>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
